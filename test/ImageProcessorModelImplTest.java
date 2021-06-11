d01014fb68fa61edde3a5237953071384f4eda35
Daniel Kim <51827691+folaroma@users.noreply.github.com>
2021-06-11 13:52:21 -0400

    more model tests

--- test/ImageProcessorModelImplTest.java
+++ test/ImageProcessorModelImplTest.java
@@ -5,6 +5,7 @@ import cs3500.imageprocessor.model.ImageProcessorModelImpl;
 import cs3500.imageprocessor.model.imageGenerating.CheckerboardGenerator;
 import cs3500.imageprocessor.model.imageGenerating.IImageGenerator;
 import cs3500.imageprocessor.model.images.ColorImpl;
+import cs3500.imageprocessor.model.images.ImageInterface;
 import java.util.ArrayList;
 import java.util.Arrays;
 import org.junit.Before;
@@ -19,6 +20,19 @@ public class ImageProcessorModelImplTest {
     testModel = new ImageProcessorModelImpl("checkerboard", new CheckerboardGenerator(2, 2,
         new ArrayList<>(Arrays.asList(new ColorImpl(255, 0, 0), new ColorImpl(0, 0, 0)))));
   }
+
+
+  // CONSTRUCTOR TESTS
+
+  // -----------------------------------------------------------------------------------------------
+
+
+  /*
+        -----------------------------
+       | Constructor Exception Tests |
+        -----------------------------
+  */
+
 
   // tests that exception is thrown when the name for a generated image in the constructor is null
   @Test(expected = IllegalArgumentException.class)
@@ -92,10 +106,197 @@ public class ImageProcessorModelImplTest {
     testModel.getImage("hello?");
   }
 
+  // testing blur id exception
+  @Test(expected = IllegalArgumentException.class)
+  public void badIdBlur() {
+    testModel.blur("a");
+  }
+
+  // testing null id blur exception
+  @Test(expected = IllegalArgumentException.class)
+  public void nullIdBlur() {
+    testModel.blur(null);
+  }
+
+  // testing sharpen id exception
+  @Test(expected = IllegalArgumentException.class)
+  public void badIdSharpen() {
+    testModel.sharpen("a");
+  }
+
+  // testing null id sharpen exception
+  @Test(expected = IllegalArgumentException.class)
+  public void nullIdSharpen() {
+    testModel.sharpen(null);
+  }
+
+
+
+  // GENERAL TESTS
+
+  // -----------------------------------------------------------------------------------------------
+
+
   //tests getting the image using its id
   @Test
   public void testGetImage() {
     assertEquals(testModel.getImage("checkerboard"), new CheckerboardGenerator(2, 2,
         new ArrayList<>(Arrays.asList(new ColorImpl(255, 0, 0), new ColorImpl(0, 0, 0)))).generateImage());
   }
+
+  // testing general blurring, entire board
+  @Test
+  public void applyBlurAll() {
+
+    assertEquals(testModel.blur("checkerboard").getPixels().get(0).get(0).getColor().getRed(), 78);
+    assertEquals(testModel.blur("checkerboard").getPixels().get(0).get(1).getColor().getRed(), 62);
+    assertEquals(testModel.blur("checkerboard").getPixels().get(1).get(0).getColor().getRed(), 62);
+    assertEquals(testModel.blur("checkerboard").getPixels().get(1).get(1).getColor().getRed(), 78);
+
+    assertEquals(testModel.blur("checkerboard").getPixels().get(0).get(0).getColor().getGreen(), 0);
+    assertEquals(testModel.blur("checkerboard").getPixels().get(0).get(1).getColor().getGreen(), 0);
+    assertEquals(testModel.blur("checkerboard").getPixels().get(1).get(0).getColor().getGreen(), 0);
+    assertEquals(testModel.blur("checkerboard").getPixels().get(1).get(1).getColor().getGreen(), 0);
+
+    assertEquals(testModel.blur("checkerboard").getPixels().get(0).get(0).getColor().getBlue(), 0);
+    assertEquals(testModel.blur("checkerboard").getPixels().get(0).get(1).getColor().getBlue(), 0);
+    assertEquals(testModel.blur("checkerboard").getPixels().get(1).get(0).getColor().getBlue(), 0);
+    assertEquals(testModel.blur("checkerboard").getPixels().get(1).get(1).getColor().getBlue(), 0);
+
+  }
+
+  // testing general blurring red
+  @Test
+  public void applyBlurRed() {
+    assertEquals(
+        testModel.blur("checkerboard").getPixels().get(0).get(1).getColor().getRed(), 62);
+  }
+
+  // testing general blurring green
+  @Test
+  public void applyBlurGreen() {
+    assertEquals(
+        testModel.blur("checkerboard").getPixels().get(0).get(1).getColor().getGreen(), 0);
+  }
+
+  // testing general blurring blue
+  @Test
+  public void applyBlurBlue() {
+    assertEquals(
+        testModel.blur("checkerboard").getPixels().get(0).get(1).getColor().getBlue(), 0);
+  }
+
+  // testing general blurring, twice red
+  @Test
+  public void applyBlurTwiceRed() {
+
+    ImageInterface checkerboardBlur = testModel.blur("checkerboard");
+    testModel.addImage("checkerboardBlur", checkerboardBlur);
+
+    assertEquals(testModel.blur("checkerboardBlur")
+        .getPixels().get(0).get(1).getColor().getRed(), 36);
+
+  }
+
+  // testing general blurring, twice green
+  @Test
+  public void applyBlurTwiceGreen() {
+
+    ImageInterface checkerboardBlur = testModel.blur("checkerboard");
+    testModel.addImage("checkerboardBlur", checkerboardBlur);
+
+    assertEquals(testModel.blur("checkerboardBlur")
+        .getPixels().get(0).get(1).getColor().getGreen(), 0);
+
+  }
+
+  // testing general blurring, twice blue
+  @Test
+  public void applyBlurTwiceBlue() {
+
+    ImageInterface checkerboardBlur = testModel.blur("checkerboard");
+    testModel.addImage("checkerboardBlur", checkerboardBlur);
+
+    assertEquals(testModel.blur("checkerboard")
+        .getPixels().get(0).get(1).getColor().getBlue(), 0);
+
+  }
+
+
+  // testing general sharpening, entire board
+  @Test
+  public void applySharpenAll() {
+
+    assertEquals(testModel.sharpen("checkerboard").getPixels().get(0).get(0).getColor().getRed(), 255);
+    assertEquals(testModel.sharpen("checkerboard").getPixels().get(0).get(1).getColor().getRed(), 126);
+    assertEquals(testModel.sharpen("checkerboard").getPixels().get(1).get(0).getColor().getRed(), 126);
+    assertEquals(testModel.sharpen("checkerboard").getPixels().get(1).get(1).getColor().getRed(), 255);
+
+    assertEquals(testModel.sharpen("checkerboard").getPixels().get(0).get(0).getColor().getGreen(), 0);
+    assertEquals(testModel.sharpen("checkerboard").getPixels().get(0).get(1).getColor().getGreen(), 0);
+    assertEquals(testModel.sharpen("checkerboard").getPixels().get(1).get(0).getColor().getGreen(), 0);
+    assertEquals(testModel.sharpen("checkerboard").getPixels().get(1).get(1).getColor().getGreen(), 0);
+
+    assertEquals(testModel.sharpen("checkerboard").getPixels().get(0).get(0).getColor().getBlue(), 0);
+    assertEquals(testModel.sharpen("checkerboard").getPixels().get(0).get(1).getColor().getBlue(), 0);
+    assertEquals(testModel.sharpen("checkerboard").getPixels().get(1).get(0).getColor().getBlue(), 0);
+    assertEquals(testModel.sharpen("checkerboard").getPixels().get(1).get(1).getColor().getBlue(), 0);
+
+  }
+
+  // testing general sharpening red
+  @Test
+  public void applySharpenRed() {
+    assertEquals(
+        testModel.sharpen("checkerboard").getPixels().get(0).get(0).getColor().getRed(), 255);
+  }
+
+  // testing general sharpening green
+  @Test
+  public void applySharpenGreen() {
+    assertEquals(
+        testModel.sharpen("checkerboard").getPixels().get(0).get(0).getColor().getGreen(), 0);
+  }
+
+  // testing general sharpening blue
+  @Test
+  public void applySharpenBlue() {
+    assertEquals(
+        testModel.sharpen("checkerboard").getPixels().get(0).get(0).getColor().getBlue(), 0);
+  }
+
+  // testing general sharpening, twice red
+  @Test
+  public void applySharpenTwiceRed() {
+    ImageInterface checkerboardBlur = testModel.blur("checkerboard");
+    testModel.addImage("checkerboardBlur", checkerboardBlur);
+
+    assertEquals(testModel.sharpen("checkerboard")
+        .getPixels().get(0).get(0).getColor().getRed(), 255);
+
+  }
+
+  // testing general sharpening, twice green
+  @Test
+  public void applySharpenTwiceGreen() {
+    ImageInterface checkerboardBlur = testModel.blur("checkerboard");
+    testModel.addImage("checkerboardBlur", checkerboardBlur);
+
+    assertEquals(testModel.sharpen("checkerboard")
+        .getPixels().get(0).get(0).getColor().getGreen(), 0);
+
+  }
+
+  // testing general sharpening, twice blue
+  @Test
+  public void applySharpenTwiceBlue() {
+    ImageInterface checkerboardBlur = testModel.blur("checkerboard");
+    testModel.addImage("checkerboardBlur", checkerboardBlur);
+
+    assertEquals(testModel.sharpen("checkerboard")
+        .getPixels().get(0).get(0).getColor().getBlue(), 0);
+
+  }
+
+
 }
