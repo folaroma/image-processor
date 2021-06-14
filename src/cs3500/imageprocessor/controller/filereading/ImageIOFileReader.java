66b740c0f655e3a5755d217d96d470dec27b41fd
Daniel Kim <51827691+folaroma@users.noreply.github.com>
2021-06-14 16:54:43 -0400

    image io reading and writing

---
+++ src/cs3500/imageprocessor/controller/filereading/ImageIOFileReader.java
@@ -0,0 +1,50 @@
+package cs3500.imageprocessor.controller.filereading;
+
+import cs3500.imageprocessor.model.images.ColorImpl;
+import cs3500.imageprocessor.model.images.IPixel;
+import cs3500.imageprocessor.model.images.ImageImpl;
+import cs3500.imageprocessor.model.images.ImageInterface;
+import cs3500.imageprocessor.model.images.PixelImpl;
+import cs3500.imageprocessor.model.images.Position2D;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.imageio.ImageIO;
+
+public class ImageIOFileReader implements IFileReader {
+
+  @Override
+  public ImageInterface readImageFromFile(String filename) {
+    File imageFile = new File(filename);
+
+    try {
+      BufferedImage image = ImageIO.read(imageFile);
+      int imageHeight = image.getHeight();
+      int imageWidth = image.getWidth();
+
+      List<ArrayList<IPixel>> pixels = new ArrayList<>();
+
+      for (int i = 0; i < imageHeight; i++) {
+        ArrayList<IPixel> row = new ArrayList<>();
+        for (int j = 0; j < imageWidth; j++) {
+          Color pixelRGB = new Color(image.getRGB(j, i));
+          row.add(new PixelImpl(new Position2D(j, i),
+              new ColorImpl(pixelRGB.getRed(), pixelRGB.getGreen(), pixelRGB.getBlue())));
+        }
+        pixels.add(row);
+      }
+
+      return new ImageImpl(pixels);
+
+
+    } catch (IOException io) {
+      throw new IllegalStateException();
+    }
+
+  }
+
+
+}
